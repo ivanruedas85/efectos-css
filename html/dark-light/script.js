b0e7a9fbc83d693b934bd3f353732eae24ad02b5
ivanruedas85 <50291974+ivanruedas85@users.noreply.github.com>
2023-04-10 06:55:52 -0600

    Delete
    Se movio a su respectiva carpeta

--- html/dark-light/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("input").addEventListener("change", () => {
-  if (document.body.className.indexOf("dark") === -1) {
-    document.body.classList.add("dark");
-  } else {
-    document.body.classList.remove("dark");
-  }
-});
