5aed327fdefe09c158a0227b0600f6f7cfe9c9b2
ivanruedas85 <ivanruedas85@gmail.com>
2023-06-22 13:01:20 -0600

    Se agregaron imagenes

---
+++ html/admin-dashboard/admin-dashboard-dark&light/js/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Javascript Tutorial',
+        ProductNumber: '85743',
+        PaymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        ProductNumber: '97245',
+        PaymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        ProductNumber: '36452',
+        PaymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
